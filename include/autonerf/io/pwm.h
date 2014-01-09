ff82f86a14b49f004c05b6a6c5e286a4ec842b1f
Ramon Kleiss <ramon@cubilon.nl>
2014-01-09 18:36:30 +0100

    Start refactoring the PWM
    Only one function left to do: pwm_set_frequency but I'm not
    really sure what's happening there...

--- include/autonerf/io/pwm.h
+++ include/autonerf/io/pwm.h
@@ -1,19 +1,31 @@
-/******************************************************************************
- * pwm.h                                                                      *
- *                                                                            *
- * Liberary to access and control the PWM pins.                               *
- *****************************************************************************/
+#ifndef AUTONERF_IO_PWM_H
+#define AUTONERF_IO_PWM_H
 
-#ifndef PWM
-#define PWM
+// Standard C dependencies
+#include <stdint.h>
 
-/* Definitions */
-#define NOCHANNALS 6
+// Defines
+#define PWM_DIRECTORY       "/sys/devices/ocp.2/pwm_test_P"
+#define PWM_FILE_DUTY       "duty"
+#define PWM_FILE_PERIOD     "period"
+#define PWM_CHANNEL_COUNT   6
 
-/* Prototypes */
+struct {
+    uint8_t     io;
+    uint8_t     header;
+    uint8_t     hpin;
+    uint8_t     pwm_pin;
+    uint16_t    suffix;
+    uint32_t    period;
+} pwm_t;
+
+extern void pwm_init(const register uint8_t io, const register uint32_t period);
+extern void pwm_set_frequency(const register uint8_t io, const register uint32_t cycle);
+extern void pwm_set_duty_cycle(const register uint8_t io, const register uint32_t cycle);
+
 void initPWM(uint8_t* io, uint32_t period);
 void setDutyCycle(uint8_t* io, uint32_t newValue);
 uint32_t freqToPeriod(uint32_t frequency);
 uint8_t calculatePWM(uint8_t chip, uint8_t pin);
 
-#endif /* PWM */
+#endif /* AUTONERF_IO_PWM_H */
--- src/io/pwm.c
+++ src/io/pwm.c
@@ -1,90 +1,65 @@
-/**
- * This file controls PWM on PWM pins.
- * !! ONLY FOR EHRPWM pins !!
- * EHR  = Enhanced High Resolution
- * ECAP = Enhanced capture
- */
-
+// Standard C dependencies
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <pwm.h>
-#include <gpio.h>
 
-#define PWMDIR "/sys/devices/ocp.2/pwm_test_P" //Complete in code
-#define PERIOD "period"
-#define DUTY   "duty"
+// Project dependencies
+#include <autonerf/logger.h>
+#include <autonerf/io/pwm.h>
+#include <autonerf/io/gpio.h>
 
-/* Structures */
-typedef struct {
-    uint8_t  header;
-    uint8_t  hpin;
-    uint8_t  pwmPin;
-    uint8_t  suffix;
-    uint8_t  io;
-    uint32_t period;
-}PWMstr;
-
-/* Local prototypes */
-uint8_t checkPin(uint8_t* io);
-void setFrequency(uint8_t* io, uint32_t* period);
-void writePWMFile(PWMstr* pinInfo, uint32_t value, char* fileName);
-
-/* Local global variables */
-PWMstr channal[NOCHANNALS] = {
-                                {0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 0}
-                             };
+struct pwm_t _channels[PWM_CHANNEL_COUNT] = {
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+};
 
 /**
- * Initializes the PWM period cycle on the pin.
+ * Writes the assigned value to the assignd filename in the PWMDIR defined by pinInfo.
  *
- * @param chip   = the chip the pin is on
- * @param pin    = the pin of the chip
- * @param period = the period of the PWM signal in nano seconds
+ * @param pinInfo  = the info required to construct the right file path
+ * @param value    = the value to write to the file
+ * @param fileName = the filename to write the int value to
  */
-void initPWM(uint8_t* io, uint32_t period)
+void
+pwm_write_file(struct pwm_t * info, const register uint32_t value, const char * file)
 {
-    //Check whether io is a PWM pin
-    if(checkPin(io) == 0){
-        printf("Faild to assign PWM pin. GPIO[%i]%i is not a PWM pin!\n", (*io / PINPCHIP), (*io % PINPCHIP));
-        fflush(stdout);
-        return;
-    }
+    FILE *  file;
+    char    filename[1024];
 
-    //Set the frequency
-    setFrequency(io, &period);
-    setDutyCycle(io, 0);
+    sprintf(filename, "%s%d_%d.%d/%s" PWM_DIRECTORY, info->header, info->hpin, info->suffix, filename);
+
+    if ((file = fopen(filename, "w")) != NULL) {
+        fprintf(file, "%d", value);
+        fclose(file);
+    } else {
+        LOG_ERROR(
+            "Failed to open the PWM file, check path: %s",
+            filename
+        );
+    }
 }
 
 /**
- * Writes a value to a PWM pin.
- * Interface to uint8_t instead of PWMstr*!
+ * Checks whether he assigned pin is a PWM pin.
+ * @param io = the io to be assigned as PWM pin
+ *
+ * @return 1 if this pin can be assigned as PWM pin
  */
-void setDutyCycle(uint8_t* io, uint32_t newValue)
+uint8_t
+pwm_check_pin(const register uint8_t io)
 {
-    uint8_t i;
+    static uint8_t  pins[] = {2, 3, 23, 22, 50, 51, 70, 71, 80, 81, 110, 111};
+    uint8_t         i      = 0;
 
-    for(i = 0;i < NOCHANNALS ;i++){
-        if(channal[i].io == *io){
-            break;
+    for(; i < (sizeof(pins) / sizeof(uint8_t)); i++){
+        if(io == pins[i]){
+            return 1;
         }
     }
 
-    if(i > NOCHANNALS){
-        printf("GPIO[%i]%i is not a PWM pin or is not initialized.\n", (*io / PINPCHIP), (*io % PINPCHIP));
-        fflush(stdout);
-        return;
-    }
-
-    //newValue = channal[i].period - newValue;
-    writePWMFile(&channal[i], newValue, DUTY);
+    return 0;
 }
 
 /**
@@ -93,21 +68,64 @@ void setDutyCycle(uint8_t* io, uint32_t newValue)
  *
  * @return the period time in nano seconds
  */
-uint32_t freqToPeriod(uint32_t frequency)
+uint32_t
+pwm_freq_to_period(const register uint32_t frequency)
 {
-    return (uint32_t)(1000000000 / frequency);
+    return (1000000000 / frequency);
 }
 
 /**
- * Calculates the required GPIO values.
- * @param chip = the IO chip of the beaglebone
- * @param pin  = the IO pin of the chip of the beaglebone
+ * Initializes the PWM period cycle on the pin.
  *
- * @return the value that represents the GPIO
+ * @param chip   = the chip the pin is on
+ * @param pin    = the pin of the chip
+ * @param period = the period of the PWM signal in nano seconds
  */
-uint8_t calculatePWM(uint8_t chip, uint8_t pin)
+void
+pwm_init(const register uint8_t io, const register uint32_t period)
 {
-    return (chip * PINPCHIP) + pin;
+    if (!pwm_check_pin(io)) {
+        LOG_ERROR(
+            "Pin %d:%d is not a PWM pin",
+            (io / GPIO_PIN_COUNT),
+            (io % GPIO_PIN_COUNT)
+        );
+    }
+
+    pwm_set_frequency(io, period);
+    pwm_set_duty_cycle(io, 0);
+}
+
+/**
+ * Writes a value to a PWM pin.
+ * Interface to uint8_t instead of PWMstr*!
+ */
+void
+pwm_set_duty_cycle(const register uint8_t io, const register uint32_t cycle)
+{
+    register uint8_t i = 0;
+
+    for (; i < PWM_CHANNEL_COUNT; i++) {
+        if (_channels[i].io == io) {
+            break;
+        }
+    }
+
+    if (i >= PWM_CHANNEL_COUNT) {
+        LOG_ERROR(
+            "GPIO[%d]%d is not a PWM pin or is not initialized",
+            (io / GPIO_PIN_COUNT),
+            (io % GPIO_PIN_COUNT)
+        );
+    }
+
+    pwm_write_file(&_channels[i], value, PWM_FILE_DUTY);
+}
+
+void
+pwm_set_frequency(const register uint8_t io, const register uint32_t period)
+{
+    LOG_EMERGENCY("Function not implemented yet: %s", __PRETTY_FUNCTION__);
 }
 
 /*******************************************************************************
@@ -185,62 +203,4 @@ void setFrequency(uint8_t* io, uint32_t* period)
 
     //Assign the period to the output
     writePWMFile(&channal[i], channal[i].period, PERIOD);
-}
-
-/**
- * Writes the assigned value to the assignd filename in the PWMDIR defined by pinInfo.
- *
- * @param pinInfo  = the info required to construct the right file path
- * @param value    = the value to write to the file
- * @param fileName = the filename to write the int value to
- */
-void writePWMFile(PWMstr* pinInfo, uint32_t value, char* fileName)
-{
-    char buff[3];
-    char buffer[100];
-    int32_t file;
-
-    //Construct adress line
-    strcpy(buffer, PWMDIR);
-    sprintf(buff, "%i", pinInfo->header);
-    strcat(buffer, buff);
-    strcat(buffer, "_");
-    sprintf(buff, "%i", pinInfo->hpin);
-    strcat(buffer, buff);
-    strcat(buffer, ".");
-    sprintf(buff, "%i", pinInfo->suffix);
-    strcat(buffer, buff);
-    strcat(buffer, "/");
-    strcat(buffer, fileName);
-
-    if((file = open(buffer, O_WRONLY)) != 0){
-        sprintf(buffer, "%i", value);
-
-        write(file, buffer, ( sizeof(buffer) / sizeof(char) ));
-        close(file);
-    } else {
-        printf("Faild to open the PWM file. Check path!\n");
-        printf("Current file path: %s\n", buffer);
-        fflush(stdout);
-    }
-}
-
-/**
- * Checks whether he assigned pin is a PWM pin.
- * @param io = the io to be assigned as PWM pin
- *
- * @return 1 if this pin can be assigned as PWM pin
- */
-uint8_t checkPin(uint8_t* io)
-{
-    uint8_t ehrpwmPins[12] = {2, 3, 23, 22, 50, 51, 70, 71, 80, 81, 110, 111}; //Calculated IO values
-    uint8_t i;
-
-    for(i = 0; i < 12; i++){
-        if(*io == ehrpwmPins[i]){
-            return 1;
-        }
-    }
-
-    return 0;
 }
