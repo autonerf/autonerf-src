e398f425d6b7f796bb4b273f68000c7357e25bb6
Ramon Kleiss <ramon@cubilon.nl>
2014-01-09 17:58:52 +0100

    Refactored the Servo

--- include/autonerf/io/servo.h
+++ include/autonerf/io/servo.h
@@ -1,15 +1,29 @@
-/******************************************************************************
- * servo.h                                                                    *
- *                                                                            *
- * API to control a servo via a hardware PWM.                                 *
- *****************************************************************************/
+#ifndef AUTONERF_IO_SERVO_H
+#define AUTONERF_IO_SERVO_H
 
-#ifndef SERVO
-#define SERVO
+// Standard C dependencies
+#include <stdint.h>
 
-/* Prototypes */
-void initServo(uint8_t* io);
-void setPos(uint8_t* io, float angle);
-uint8_t calculateServo(uint8_t chip, uint8_t pin);
+// Defines
+#define SERVO_VALUE_MIN     520000
+#define SERVO_VALUE_MAX     1820000
+#define SERVO_DELTA_TIME    1300000.0f
 
-#endif /* SERVO */
+/**
+ Assign a PWM signal to a servo motor
+
+ @param io The number of the I/O pin to assign
+ @warning  *Warning:* The `io` parameter must be calculated using the `gpio_calculate` function
+ @see      gpio_calculate
+ */
+extern void servo_init(const register uint8_t io);
+
+/**
+ Set a servo to a specific angle
+
+ @param io    The servo I/O pin to use
+ @param angle The angle to set the servo to (in degrees)
+ */
+extern void servo_set_position(const register uint8_t io, const register float angle);
+
+#endif /* AUTONERF_IO_SERVO_H */
